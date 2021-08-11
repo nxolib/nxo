6b132e1d9536471334a8820e1573489e5c9c4ca6
Bunny Lushington <bunny@bapi.us>
2021-08-11 09:55:20 -0400

    org removal working

--- src/nxo_pages/nxopage_user_form.erl
+++ src/nxo_pages/nxopage_user_form.erl
@@ -40,9 +40,27 @@ event({new_org_form, UserID}) ->
       Form = org_form(OrgAbbrv, UserID),
       wf:replace(new_org_dropdown, dropdown({additional_orgs, UserID})),
       wf:insert_bottom(org_forms, Form)
-  end.
+  end;
+event({org_cancel, UserID, OrgAbbrv}) ->
+  wf:state(selected_orgs,
+           lists:delete(wf:to_binary(OrgAbbrv), ?SELECTED_ORGS_DEFAULT)),
+  case existing_global_roles(UserID) of
+    [] -> ok; %% new user, nothing to update & persist
+    GlobalRoles ->
+      RemainingOrgs = lists:delete(OrgAbbrv, wf:qs(selected_new_org)),
+      persist_orgs(UserID, RemainingOrgs, GlobalRoles)
+  end,
+  wf:replace(new_org_dropdown, dropdown({additional_orgs, UserID})),
+  wf:remove(wf:to_atom(OrgAbbrv ++ "_panel")).
 
 
+
+
+button({org_cancel, UserID, OrgAbbrv}) ->
+  #button{ postback={org_cancel, UserID, OrgAbbrv},
+           delegate=?MODULE,
+           class="btn btn-link",
+           text="Remove from Organization" };
 button({submit, UserID}) ->
   {Text, ID} = case UserID of
                  [] -> {"Add User", nxo:uuid()};
@@ -72,8 +90,10 @@ dropdown({additional_orgs, UserID}) ->
                             end, nxo_org:all()),
   case Options of
     [] ->
-      wf:remove(new_org);
+      wf:wire(new_org, #hide{}),
+      #dropdown{ id=new_org_dropdown, class="d-none" };
     _ ->
+      wf:wire(new_org, #show{}),
       #dropdown{ id=new_org_dropdown,
                  class="custom-select",
                  postback={new_org_form, UserID},
@@ -83,35 +103,42 @@ dropdown({additional_orgs, UserID}) ->
 
 submission(UserID) ->
   %% UserID might be from the DB or it might have been generated.
-  %% XXX: This really requires further refinement.  Two things are weird:
-  %%
-  %%  1. I think user_add and user_set_roles should probably be a
-  %%     transaction, and
-  %%
-  %%  2. We set the GlobalRoles twice.  This might not be all bad (I
-  %%     suspect user_add will be used elsewhere).
   case nxo_validate:validate(user_form, #{user_id => UserID}) of
     true ->
       SimpleParams = nxo_datamap:apply(user_add),
       GlobalRoles = [ wf:to_binary(R) || R <- wf:qs(global_roles) ],
       Params = [UserID | SimpleParams] ++ [GlobalRoles],
       nxo_db:q(user_add, Params),
-      OrgRoles =
-        lists:map(fun(Abbrv) ->
-                      Title = wf:q(wf:to_atom(Abbrv ++ "_title")),
-                      Contact = wf:q(wf:to_atom(Abbrv ++ "_is_contact")),
-                      Roles = [ wf:to_binary(R)
-                                || R <- wf:qs(wf:to_atom(Abbrv ++ "_role")) ],
-                      nxo_db:q(user_add_org, [UserID, Abbrv,
-                                              wf:to_atom(Contact), Title]),
-                      Roles
-                  end, wf:qs(selected_new_org)),
-      AllRoles = lists:flatten([GlobalRoles, OrgRoles]),
-      nxo_db:q(user_set_roles, [UserID, AllRoles]),
+      persist_orgs(UserID, wf:qs(selected_new_org), GlobalRoles),
       wf:redirect("/users");
     false ->
       nxo_view:report_validation_failure()
   end.
+
+existing_global_roles([]) -> [];
+existing_global_roles(UserID) ->
+  lists:filter(fun(R) ->
+                   case binary:match(R, <<"global::">>) of
+                     nomatch -> false;
+                     _       -> true
+                   end end, nxo_roles:all(UserID)).
+
+persist_orgs(UserID, Orgs, GlobalRoles) ->
+  OrgRoles =
+    lists:map(fun(Abbrv) ->
+                  Title = wf:q(wf:to_atom(Abbrv ++ "_title")),
+                  Contact = wf:q(wf:to_atom(Abbrv ++ "_is_contact")),
+                  Roles = [ wf:to_binary(R)
+                            || R <- wf:qs(wf:to_atom(Abbrv ++ "_role")) ],
+                  nxo_db:q(user_add_org, [UserID, Abbrv,
+                                          wf:to_atom(Contact), Title]),
+                  Roles
+              end, Orgs),
+  AllRoles = lists:flatten([GlobalRoles, OrgRoles]),
+  OrgList = [ wf:to_binary(O) || O <- Orgs ],
+  nxo_db:q(user_remove_other_orgs, [UserID, OrgList]),
+  nxo_db:q(user_set_roles, [UserID, AllRoles]).
+
 
 org_form("global", _) ->
   [];
@@ -132,4 +159,5 @@ org_form(OrgAbbrv, UserID) ->
   Data = maps:merge(User, #{ org => Org,
                              user_org =>  UserOrg,
                              non_global_groups => Groups }),
+  ?PRINT(Data),
   #template{ text=nxo_template:pretty_render(user_org_form, Data) }.
