05133a801e683c430cbfe983006fd83dcdf9fd85
Bunny Lushington <bunny@bapi.us>
2021-08-26 08:55:37 -0400

    directory user facing functions filled in

--- src/nxo_directory.erl
+++ src/nxo_directory.erl
@@ -9,7 +9,8 @@
         , any_defined/0
         , directory_organizations/0
          %% functions to use the directory
-        , search/2
+        , search/3
+        , authenticate/3
         , open/1
         ]).
 
@@ -29,10 +30,90 @@ any_defined() ->
 directory_organizations() ->
   nxo_db:q(directory_organizations).
 
-search(Term, OrgAbbrv) ->
-  DirMaps = find(OrgAbbrv),
+search(account, Ident, OrgAbbrv) ->
+  lists:flatten(search_all_directories(account, Ident, OrgAbbrv));
 
+search(entry, Mail, OrgAbbrv) ->
+  case lists:flatten(search_all_directories(entry, Mail, OrgAbbrv)) of
+    [] -> not_found;
+    Res -> {entry, Res}
+  end.
 
+authenticate(Mail, Password, OrgAbbrv) ->
+  case search(entry, Mail, OrgAbbrv) of
+    not_found ->
+      false;
+    {entry, Entries} ->
+      case lists:search(fun (E) -> authenticate_entry(E, Password) end,
+                        Entries) of
+        false -> false;
+        {value, _} -> true
+      end
+  end.
+
+authenticate_entry(Entry, Password) ->
+  {OrgAbbrv, Directory} = maps:get(directory, Entry),
+  DN = maps:get(dn, Entry),
+  case find(OrgAbbrv, Directory) of
+    [] -> false;
+    [DirMap] ->
+      case open(DirMap) of
+        {false, _} -> false;
+         Handle -> bind(Handle, DN, Password)
+      end
+  end.
+
+search_all_directories(Type, Ident, OrgAbbrv) ->
+  [ directory_search(Type, Ident, DirMap) || DirMap <- find(OrgAbbrv) ].
+
+directory_search(account, Ident, DirMap) ->
+  case open(DirMap) of
+    {false, _} ->
+      [];
+    Handle ->
+      Filter = filter(account, Ident, DirMap),
+      Attrs = attributes(account, DirMap),
+      {ok, {eldap_search_result, Entries, _}} =
+        eldap:search(Handle, [{base, base(DirMap)}, Filter, Attrs]),
+      result_to_map(account, Entries, DirMap)
+  end;
+
+directory_search(entry, Email, DirMap) ->
+  case open(DirMap) of
+    {false, _} ->
+      [];
+    Handle ->
+      Filter = filter(entry, Email, DirMap),
+      Attrs = attributes(account, DirMap),
+      {ok, {eldap_search_result, Entries, _}} =
+        eldap:search(Handle, [{base, base(DirMap)}, Filter, Attrs]),
+      result_to_map(account, Entries, DirMap)
+  end.
+
+result_to_map(account, Result, DirMap) ->
+  Directory = directory(DirMap),
+  [UIDAttr, FNameAttr, LNameAttr, MailAttr] =
+    [ attr(A, DirMap) || A <- [uid, fname, lname, mail] ],
+  [ #{ uid =>   hd(proplists:get_value(UIDAttr, R,   [[]])),
+       mail =>  hd(proplists:get_value(MailAttr, R,  [[]])),
+       fname => hd(proplists:get_value(FNameAttr, R, [[]])),
+       lname => hd(proplists:get_value(LNameAttr, R, [[]])),
+       dn => DN,
+       directory => Directory } || {eldap_entry, DN, R} <- Result ].
+
+filter(entry, Ident, DirMap) ->
+  {filter, eldap:equalityMatch(attr(mail, DirMap), Ident)};
+
+filter(account, Ident, DirMap) ->
+  F1 = eldap:'or'([eldap:substrings(attr(lname, DirMap), [{initial, Ident}]),
+                   eldap:substrings(attr(mail, DirMap), [{initial, Ident}]),
+                   eldap:substrings(attr(uid, DirMap), [{initial, Ident}])]),
+  {filter, eldap:'and'([F1,
+                        eldap:present(attr(mail, DirMap)),
+                        eldap:present(attr(uid, DirMap))])}.
+
+attributes(account, DirMap) ->
+  {attributes, [ attr(A, DirMap) || A <- [uid, mail, fname, lname] ]}.
 
 open(DirMap) ->
   case eldap:open([host(DirMap)], [{port, port(DirMap)}]) of
@@ -59,7 +140,7 @@ maybe_start_tls(Handle, TLSVersions) ->
       error(cannot_start_ldap_tls, [Error])
   end.
 
-bind(Handle, [], []) ->
+bind(_Handle, [], []) ->
   no_userpass_supplied;
 bind(Handle, User, Pass) ->
   eldap:simple_bind(Handle, User, Pass) == ok.
@@ -88,6 +169,9 @@ attr(uid, DirMap) ->
 attr(mail, DirMap) ->
   wf:to_list(maps:get(<<"attr_mail">>, DirMap));
 attr(fname, DirMap) ->
-  wf:to_list(maps:get(<<"attr_fname">> DirMap));
+  wf:to_list(maps:get(<<"attr_fname">>, DirMap));
 attr(lname, DirMap) ->
-  wf:to_list(maps:get(<<"attr_lname">> DirMap)).
+  wf:to_list(maps:get(<<"attr_lname">>, DirMap)).
+
+directory(DirMap) ->
+  {maps:get(<<"org_abbrv">>, DirMap), maps:get(<<"directory">>, DirMap)}.
