55c9633ec6fa1d91aaa5500f9d8c467edc3be170
Bunny Lushington <bunny@bapi.us>
2022-06-02 15:25:10 -0500

    reverted debugging

--- src/nxo_sup.erl
+++ src/nxo_sup.erl
@@ -10,9 +10,9 @@ start_link() ->
 
 init([]) ->
   nxo_db:start(),
-  % nxo_db:apply_full_ddl(),
+  nxo_db:apply_full_ddl(),
   nxo_template:compile_all(),
-  % nxo_settings:init(),
+  nxo_settings:init(),
 
   %% We're only allowed one document_root yet we might need to serve
   %% up files from priv_dir(nxo)/nxostatic or priv_dir(app)/static.
